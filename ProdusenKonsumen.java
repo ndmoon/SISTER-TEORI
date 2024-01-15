c08eb07c4a47ab78e1b6c11931147f25e239dbd0
nadiamonika <115436702+ndmoon@users.noreply.github.com>
2024-01-15 22:13:23 +0700

    Add files via upload

---
+++ ProdusenKonsumen.java
@@ -0,0 +1,48 @@
+/*
+	Nama : Nadia Monika Putri
+	NIM : 2111082034
+	Kelas : TRPL 3B
+*/
+
+class SharedData {
+    int data;
+    synchronized void set(int value) {
+        System.out.println("Generate " + value);
+        data = value;
+    }
+    synchronized int get() {
+        System.out.println("Get " + data);
+        return data;
+    }
+}
+class Producer implements Runnable {
+    SharedData sd;
+    Producer(SharedData sd) {
+        this.sd = sd;
+        new Thread(this, "Producer").start();
+    }
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            sd.set((int)(Math.random()*100));
+        }
+    }
+}
+class Consumer implements Runnable {
+    SharedData sd;
+    Consumer(SharedData sd) {
+        this.sd = sd;
+        new Thread(this, "Consumer").start();
+    }
+    public void run() {
+        for (int i = 0; i < 10 ; i++) {
+            sd.get();
+        }
+    }
+}
+class ProdusenKonsumen {
+    public static void main(String args[]) throws Exception {
+        SharedData sd = new SharedData();
+        new Producer(sd);
+        new Consumer(sd);
+    }
+}
---
+++ Runnable1.java
@@ -0,0 +1,27 @@
+/*
+	Nama : Nadia Monika Putri
+	NIM : 2111082034
+	Kelas : TRPL 3B
+*/
+
+class PrintNameThread implements Runnable {
+    Thread thread;
+    PrintNameThread(String name) {
+        thread = new Thread(this, name);
+        thread.start();
+    }
+    public void run() {
+        String name = thread.getName();
+        for (int i = 0; i < 100; i++) {
+            System.out.print(name);
+        }
+    }
+}
+class Runnable1 {
+    public static void main(String args[]) {
+    new PrintNameThread("A");
+    new PrintNameThread("B");
+    new PrintNameThread("C");
+    new PrintNameThread("D");
+    }
+}
